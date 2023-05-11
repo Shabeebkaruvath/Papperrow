62b5d6ea82612e332a7f1a8bf6cec572e3b9544e
shabeeb <shabeebkaruvath@gmail.com>
2023-05-11 12:49:30 +0530

    navbar-done

---
+++ src/content/Navbar/Navbar.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import cart from "./cart.png";
+import search from "./search.png";
+import account from "./male.png";
+import menu from "./menu.png";
+import close from "./close.png";
+
+function Navbar() {
+  const [change, setChange] = useState(true);
+
+  return (
+    <div>
+      <div className={change ? "nav-main" : "nav-main2"}>
+        <div className="nav-1">
+          <h1 className="logo">PapeRow</h1>
+        </div>
+        <div className="nav-2">
+          <ul>
+            <li>Home</li>
+            <li>Books</li>
+            <li>Blog</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+        <div className="nav-3">
+          <div className="nav-31">
+            <img src={search} alt="search" className="search-icon" />
+            <img src={cart} alt="cart" className="cart-icon" />
+            <div className="ac-img">
+              <img src={account} alt="account" className="account-icon" />
+            </div>
+            <img
+              src={change ? menu : close}
+              alt="menu"
+              className="menu-icon"
+              onClick={() => {
+                setChange(!change);
+              }}
+            />
+          </div>
+        </div>
+      </div>
+      <div>
+      <div className={change ? "nope":"nav-2-mobile"}>
+          <ul>
+            <li>Home</li>
+            <li>Books</li>
+            <li>Blog</li>
+            <li id="nca">Contact Us</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
